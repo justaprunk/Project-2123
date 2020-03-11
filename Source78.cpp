24b10e7512249aef87bc406119c368297dde0f45
justaprunk <61222962+justaprunk@users.noreply.github.com>
2020-03-11 10:09:10 +0300

    Update Source78.cpp

--- Source78.cpp
+++ Source78.cpp
@@ -1,28 +1,36 @@
 #include <iostream>
 using namespace std;
 
+int LinearSearch(int value, int* array);
+	
 int main()
 {
-	int const n = 4; //раздел переменных
+	int const n = 4;
 	int value;
 	int array[n];
 
 
-	cout << "Enter value:" << endl; //ввод значения для проверки
+	cout << "Enter value:" << endl;
 	cin >> value;
 
-	cout << "Enter the array:" << endl; //ввод массива
+	cout << "Enter the array:" << endl;
 	for (int i = 0; i < n; i++)
 	{
 		cin >> array[i];
 	}
-
-	for (int i = 0; i < n; i++) //цикл проверки массива на наличие заданного числа
+cout << LinearSearch(value, array) << endl;
+};
+	
+int LinearSearch(int value, int* array)
+{
+	int n = sizeof;
+	
+	for (int i = 0; i < n; i++)
 	{
 		if (array[i] == value)
 		{
 			return i;
 		}
-	};
+	}
 	return -1;
 }
